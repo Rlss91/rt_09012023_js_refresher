2e41fdf3bacd5aea364d93259064ddc3f2330d02
Rlss91 <rlss91@gmail.com>
2023-01-11 15:56:56 +0200

    extends

--- classes/Animal.js
+++ classes/Animal.js
@@ -7,6 +7,9 @@ class Animal {
     this.age = age;
     this.color = color;
   }
+  get nameAndColor() {
+    return this.name + " " + this.color;
+  }
 }
 
 class Dog extends Animal {
@@ -23,9 +26,41 @@ class Dolphin extends Animal {
     super(name, age, color);
     this.group = group;
   }
+  get nameAndColor() {
+    return 2;
+  }
 }
 
 let dog = new Dog("Kenny");
 console.log("🚀 ~ file: Animal.js:27 ~ dog", dog);
 
 let dolphin = new Dolphin("flipper", 10, "#00f", "Bardugo family");
+
+console.log(dolphin instanceof Animal);
+console.log(dolphin.constructor.name);
+console.log("nameAndColor", dolphin.nameAndColor);
+const printAnimal = (animal) => {
+  if (!animal) {
+    return;
+  }
+  if (!(animal instanceof Animal)) {
+    return;
+  }
+  console.log("animal: ", animal.name, animal.age, animal.color);
+};
+
+printAnimal(dog);
+printAnimal(dolphin);
+printAnimal();
+printAnimal({});
+
+/*
+    let d = {
+        name;
+        age;
+        color;
+        group;
+        nameAndColor(from animal)...
+    }
+    d.nameAndColor = nameAndColor(from Dolphin)
+*/
